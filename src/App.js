1e532501290b45377115ee2bcd583b8c37526efe
Arif Ilham Maulana <76241878+Grydine@users.noreply.github.com>
2022-03-24 19:43:41 +0700

    push tgs 2 module 2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import "./App.css";
 import data from "./data/single-sample";
+import { TrackComponent } from "./components";
 
 function App() {
   return (
     <section>
       <div className="container">
-        <div className="song">
-          <div className="song-img">
-            <img src={data.album.images[0].url} className="App-logo" alt="logo" />
-          </div>
-          <div className="song-desc">
-            <p>{data.album.name}</p>
-            <p>{data.artists[0].name}</p>
-            <button>Select</button>
-          </div>
-        </div>
+        <TrackComponent image={data.album.images[0].url} title={data.album.name} artist={data.artists[0].name} />
       </div>
     </section>
   );
