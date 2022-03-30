60d5910c4cb28e2b7be7eabe32ba003ca4552849
Arif Ilham Maulana <76241878+Grydine@users.noreply.github.com>
2022-03-30 17:28:32 +0700

    commit 3 tgs 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import "./App.css";
-import data from "./data/all-sample";
-import TrackComponent from "./components/track/track.component";
+import Home from "./pages/home/home.component";
 
 function App() {
   return (
     <section>
       <div className="container-song">
-        {data.map((x) => {
-          return <TrackComponent key={x.album.id} image={x.album.images[0].url} title={x.album.name} artist={x.artists[0].name} />;
-        })}
+        <Home />
       </div>
     </section>
   );
