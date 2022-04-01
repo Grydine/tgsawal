9a5c3f644a5ed2d8a40231e6a77bb7565fc8d9b1
Arif Ilham Maulana <76241878+Grydine@users.noreply.github.com>
2022-04-01 16:33:14 +0700

    co tg 3 m 3

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import { SearchProvider } from "./context/useSearchResult";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SearchProvider>
+      <App />
+    </SearchProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
