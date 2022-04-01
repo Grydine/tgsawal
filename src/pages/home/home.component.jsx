1c03a6f6676c7333ebb791c6ee3d047174adfc6e
Arif Ilham Maulana <76241878+Grydine@users.noreply.github.com>
2022-04-01 16:32:23 +0700

    c t 3 m 3

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -1,118 +1,87 @@
-import React, { useEffect } from "react";
-// import "../spotify/Spotify.css";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import TrackComponent from "../../components/track/track.component";
+import { useSearchResult } from "../../context/useSearchResult";
 
-const BASE_URL = "https://api.spotify.com/v1";
-const SPOTIFY_KEY = process.env.REACT_APP_SPOTIFY_KEY;
-const REDIRECT_URI = "http://localhost:3000/callback/";
+const BASE_URL = process.env.REACT_APP_SPOTIFY_BASE_URL;
+const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
 const SCOPE = "playlist-modify-private";
-const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
+const AUTHORIZE_URL = process.env.REACT_APP_SPOTIFY_AUTHORIZE_LINK;
 
-class Home extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      isLoggedIn: false,
-      savedToken: "",
-      searchQuery: "",
-      searchResult: [],
-    };
+const Home = () => {
+  const [token, setToken] = useState(null);
+  const [query, setQuery] = useState("");
+  const { result, setResult, selectedSongs } = useSearchResult();
 
-    this.onSubmit = this.onSubmit.bind(this);
-    this.onSearch = this.onSearch.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.onClick = this.onClick.bind(this);
-  }
+  const handleAuthorizeUser = () => {
+    window.location.replace(`${AUTHORIZE_URL}?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}`);
+  };
 
-  componentDidMount() {
-    console.warn("didMount");
-    const access_token = new URLSearchParams(window.location.hash).get("#access_token");
-    this.setState(
-      {
-        savedToken: access_token,
-        isLoggedIn: true,
-      },
-      () => console.log(this.state.savedToken)
-    );
-  }
+  const parseToken = (url) => {
+    const parsed = url.split("&")[0].split("=");
+    const token = parsed[parsed.length - 1] ?? null;
+    setToken(token);
+  };
 
-  handleChange(event) {
-    this.setState({
-      searchQuery: event.target.value,
-    });
-  }
-
-  onSearch(event) {
-    axios
-      .get("https://api.spotify.com/v1/search", {
-        headers: {
-          Authorization: `Bearer ${this.state.savedToken}`,
-        },
+  const handleSearch = async () => {
+    const response = await axios
+      .get(`${BASE_URL}search`, {
         params: {
-          q: `${this.state.searchQuery}`,
+          q: query,
           type: "track",
+        },
+        headers: {
+          Authorization: `Bearer ${token}`,
         },
       })
       .then((response) => {
-        const data = response.data.tracks.items;
-        console.log(data);
-        this.setState({
-          searchResult: data,
-        });
-      })
-      .catch((error) => {
-        console.log(error);
+        setResult(response.data.tracks.items);
       });
-    event.preventDefault();
-  }
+  };
 
-  onSubmit(event) {
-    const url = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${SPOTIFY_KEY}&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}&response_type=token&show_dialog=true`;
-    window.location = url;
-    event.preventDefault();
-  }
+  useEffect(() => {
+    if (window.location.hash) parseToken(window.location.hash);
+  }, []);
 
-  onClick(event) {
-    this.setState({
-      isLoggedIn: false,
-      savedToken: "",
-    });
-    window.location.replace = "http://localhost:3000/";
-  }
-
-  render() {
-    let element;
-    if (this.state.isLoggedIn === true && this.state.savedToken != null) {
-      element = (
-        <form onSubmit={this.onSearch}>
-          <input type="text" id="inpuText" onChange={this.handleChange} />
-          <button type="submit" value="submit">
-            Search
-          </button>
-          <button onClick={this.onClick}>Back to home</button>
-          {this.state.searchResult.map((item, index) => (
-            <TrackComponent image={item.album.images[0].url} title={item.album.name} artist={item.artists[0].name} alt="Image not loaded" key={index} />
-          ))}
-        </form>
-      );
-    } else
-      element = (
-        <form onSubmit={this.onSubmit}>
-          <p className="title">Click the button to Login</p>
-          <button type="submit" value="submit">
-            Submit
-          </button>
-        </form>
-      );
-
-    document.onkeydown = function () {
-      if (window.event.keyCode === "13") {
-        this.onSearch();
-      }
-    };
-    return <div className="spotify-track">{element}</div>;
-  }
-}
+  return (
+    <>
+      {!token && <button onClick={handleAuthorizeUser}>Login</button>}
+      {token && (
+        <>
+          {result.length > 0 && (
+            <button
+              onClick={() => {
+                setResult([]);
+                setQuery("");
+              }}
+            >
+              Clear Result
+            </button>
+          )}
+          <input
+            type="text"
+            name="query"
+            id="query"
+            value={query}
+            onChange={(e) => {
+              setQuery(e.target.value);
+            }}
+          />
+          <button onClick={handleSearch}>Search</button>
+        </>
+      )}
+      {selectedSongs.length > 0 && (
+        <div>
+          <h1>Selected Songs</h1>
+          {selectedSongs.map((song, index) => {
+            return <TrackComponent key={song.uri} number={index} title={song.name} artist={song.artists[0].name} uri={song.uri} image={song.album.images[0].url} />;
+          })}
+        </div>
+      )}
+      {result.length > 0 && result.map((song, index) => <TrackComponent key={index} number={index} title={song.name} artist={song.artists[0].name} uri={song.uri} image={song.album.images[0].url} />)}
+    </>
+  );
+};
 
 export default Home;
